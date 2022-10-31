c69e03516dc0525b11ab0d3f112f31bcb261a007
Aid Puska <aidpuska29@gmail.com>
2022-10-31 18:54:10 +0100

    node_modules and .env gone!

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
-//const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -47,11 +46,13 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisisasecret'
+
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisisasecret'
+        secret
     }
 });
 
@@ -62,7 +63,7 @@ store.on('error', function(e) {
 const sessionConfig = {
     store,
     name: '_isd',
-    secret: 'thisisasecret',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
